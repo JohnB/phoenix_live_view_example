f2f087ab325d8ef0b65365797535ef0f8f6bd4b6
JohnB <john.baylor@gmail.com>
2019-08-25 13:29:25 -0700

    Enlarge the board and add a pentomino.

--- lib/demo_web/live/game.ex
+++ lib/demo_web/live/game.ex
@@ -1,5 +1,6 @@
 defmodule DemoWeb.Game do
   use Phoenix.LiveView
+  alias TilingGame.Pentomino
 
   def render(assigns) do
     ~L"""
@@ -8,16 +9,36 @@ defmodule DemoWeb.Game do
           <td class="<%= @board["0"] %>"></td>
           <td class="<%= @board["1"] %>"></td>
           <td class="<%= @board["2"] %>"></td>
-        </tr>
-        <tr>
           <td class="<%= @board["3"] %>"></td>
           <td class="<%= @board["4"] %>"></td>
-          <td class="<%= @board["5"] %>"></td>
         </tr>
         <tr>
+          <td class="<%= @board["5"] %>"></td>
           <td class="<%= @board["6"] %>"></td>
           <td class="<%= @board["7"] %>"></td>
           <td class="<%= @board["8"] %>"></td>
+          <td class="<%= @board["9"] %>"></td>
+        </tr>
+        <tr>
+          <td class="<%= @board["10"] %>"></td>
+          <td class="<%= @board["11"] %>"></td>
+          <td class="<%= @board["12"] %>"></td>
+          <td class="<%= @board["13"] %>"></td>
+          <td class="<%= @board["14"] %>"></td>
+        </tr>
+        <tr>
+          <td class="<%= @board["15"] %>"></td>
+          <td class="<%= @board["16"] %>"></td>
+          <td class="<%= @board["17"] %>"></td>
+          <td class="<%= @board["18"] %>"></td>
+          <td class="<%= @board["19"] %>"></td>
+        </tr>
+        <tr>
+          <td class="<%= @board["20"] %>"></td>
+          <td class="<%= @board["21"] %>"></td>
+          <td class="<%= @board["22"] %>"></td>
+          <td class="<%= @board["23"] %>"></td>
+          <td class="<%= @board["24"] %>"></td>
         </tr>
       </table>
     </div>
@@ -25,9 +46,9 @@ defmodule DemoWeb.Game do
   end
 
   def mount(_session, socket) do
-    {:ok, assign(socket, board: %{
-      "0" => "red",
-      "4" => "blue",
-    })}
+    {:ok, pentomino} = Pentomino.piece(17)
+    board = pentomino |> Map.new(fn idx -> {Integer.to_string(idx), "green"} end)
+
+    {:ok, assign(socket, board: board)}
   end
 end
